b1aa687664eef2013b5af98482ba492e185e2a9c
asmitsaha5 <asmitsaha5@gmail.com>
2024-10-31 15:43:11 +0530

    ...

---
+++ Basics/MadLibs.py
@@ -0,0 +1,7 @@
+color = input("Enter a color : ")
+plural_Noun = input("Enter a plural Noun :")
+celebrity = input("Enter a name of a celebrity :")
+
+print("Roses are "+ color)
+print(plural_Noun + " are blue")
+print("I love " + celebrity)
---
+++ Basics/Variables&dataTypes.py
@@ -0,0 +1,8 @@
+character_name = "Asmit"
+character_age = "20" 
+print("There was a man named " + character_name + ",")
+print("He was " + character_age + " years old.")
+character_name = "Aranya"
+character_age = "21"
+print("He really liked the name " + character_name + ",")
+print("But he didn't liked being " + character_age + ".")
