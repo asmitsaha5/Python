9c8077e0e23689cb4011caea98012eddb43cac91
asmitsaha5 <asmitsaha5@gmail.com>
2024-11-03 00:01:25 +0530

    ..

---
+++ Basics/Functions.py
@@ -0,0 +1,7 @@
+def sayhi(name, age):
+    print ("Hello " + name + ", you're " + str(age)) 
+
+
+name = input("Enter your name: ")
+age = int(input("Enter your age : "))
+sayhi(name, age)
---
+++ Basics/Tuples.py
@@ -0,0 +1,5 @@
+coordinates1 = (6,7)
+coordinates2 = [(12,32) , (67,21)]
+
+print(coordinates1)
+print(coordinates2)
